a7ffa6f988d74b3d05973cf512b580acd43f5c58
Your Name <you@example.com>
2021-08-04 23:25:53 +0200

    a lot of work has been done

--- app/src/main/java/com/example/firsttry/MainActivity.java
+++ app/src/main/java/com/example/firsttry/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.firsttry;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.DocumentsContract;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -20,13 +30,28 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.concurrent.ThreadLocalRandom;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.firsttry.MESSAGE";
+    private static final int CREATE_FILE = 1;
+    private static final int OPEN_REQUEST_CODE = 41;
+    private static final int WRITE_REQUEST_CODE = 101;
 
     TextView titlepage,subtitlepage,btexercise;
     ImageView picstatistic;
@@ -170,6 +195,164 @@ public class MainActivity extends AppCompatActivity {
                 puWindow.showAsDropDown(pubtnadd, 0, 0);
             }
         });
+
+        final ImageButton pubtnwrite = (ImageButton) findViewById(R.id.buttonwrite);
+        pubtnwrite.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                View puView = layoutInflater.inflate(R.layout.popup_read_write, null);
+                puView.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.popup_show));
+
+                TextView text = (TextView) puView.findViewById(R.id.text_id);
+                text.setText("Write database to file?");
+
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                final PopupWindow puWindow = new PopupWindow(puView,height,width,true);
+                puWindow.showAtLocation(arg0, Gravity.CENTER, 0, 0);
+                puWindow.setAnimationStyle(R.style.Animation);
+
+                Button btnWrite = (Button) puView.findViewById(R.id.button_write);
+                btnWrite.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        writeDataBase();
+                        puWindow.dismiss();
+                    }
+                });
+                puWindow.showAsDropDown(pubtnload, 0, 0);
+            }
+        });
+
+        final ImageButton pubtnread = (ImageButton) findViewById(R.id.buttonread);
+        pubtnread.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                View puView = layoutInflater.inflate(R.layout.popup_read_write, null);
+                puView.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.popup_show));
+
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                final PopupWindow puWindow = new PopupWindow(puView,height,width,true);
+                puWindow.showAtLocation(arg0, Gravity.CENTER, 0, 0);
+                puWindow.setAnimationStyle(R.style.Animation);
+
+                Button btnRead = (Button) puView.findViewById(R.id.button_write);
+                btnRead.setText("READ");
+                btnRead.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        readDataBase();
+                        puWindow.dismiss();
+                    }
+                });
+                puWindow.showAsDropDown(pubtnload, 0, 0);
+                Toast.makeText(MainActivity.this, "read database from file", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void writeDataBase() {
+        // when you create document, you need to add Intent.ACTION_CREATE_DOCUMENT
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        // filter to only show openable items.
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        // Create a file with the requested Mime type
+        intent.setType("text/plain");
+        startActivityForResult(intent, WRITE_REQUEST_CODE);
+    }
+
+    private void readDataBase() {
+        // when you create document, you need to add Intent.ACTION_CREATE_DOCUMENT
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        // filter to only show openable items.
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        // Create a file with the requested Mime type
+        intent.setType("text/plain");
+        startActivityForResult(intent, OPEN_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == WRITE_REQUEST_CODE) {
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                    if (data != null
+                            && data.getData() != null) {
+                        writeInFile(data.getData());
+                    }
+                    break;
+                case Activity.RESULT_CANCELED:
+                    break;
+            }
+        } else if (requestCode == OPEN_REQUEST_CODE) {
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                if (data != null
+                        && data.getData() != null) {
+                    readFromFile(data.getData());
+                }
+                break;
+                case Activity.RESULT_CANCELED:
+                    break;
+            }
+        }
+    }
+
+    private void readFromFile(@NonNull Uri uri) {
+        OutputStream outputStream;
+        StringFormatter stringFormatter = new StringFormatter();
+        try {
+            String toRead;
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            toRead = reader.readLine();
+            stringFormatter.setWodList(toRead);
+            inputStream.close();
+            ArrayList<Workout> wodList = stringFormatter.getWodList();
+            DatabaseHelper dbhandler = DatabaseHelper.getInstance(this);
+            for (int i=0; i<wodList.size(); i++) {
+                ArrayList<Exercise> exeList = new ArrayList<>();
+                int id = (int) dbhandler.addOrUpdateWorkout(wodList.get(i));
+                wodList.get(i).setID(id);
+                dbhandler.removeExercises(wodList.get(i));
+                exeList = wodList.get(i).getExercises();
+                for (int j=0; j<exeList.size(); j++) {
+                    dbhandler.addExerciseInWorkout(exeList.get(j),wodList.get(i));
+                    ExerciseDetail exedet = new ExerciseDetail(exeList.get(j).getName(),1);
+                    dbhandler.addOrUpdateExercise(exedet);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeInFile(@NonNull Uri uri) {
+        OutputStream outputStream;
+        try {
+            String toWrite = "";
+            DatabaseHelper dbhandler = DatabaseHelper.getInstance(this);
+            ArrayList<Workout> wodList = dbhandler.loadDatabase();
+
+            StringFormatter stringFormatter = new StringFormatter();
+            for (int i = 0; i < wodList.size(); i++) {
+                stringFormatter.setContent(wodList.get(i));
+                toWrite += stringFormatter.getContent();
+            }
+            outputStream = getContentResolver().openOutputStream(uri);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream));
+            bw.write(toWrite);
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void openStringEditor(View view) {
@@ -205,15 +388,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     public void openEditor(View view) {
         Intent intent = new Intent(this, AddWorkout2.class);
         startActivity(intent);
     }
 
-
     public void resetDatabase(View view) {
-
         DatabaseHelper dbhandler = DatabaseHelper.getInstance(this);
         dbhandler.deleteDatabase();
     }
@@ -256,11 +436,12 @@ public class MainActivity extends AppCompatActivity {
             exeList = wodList.get(i).getExercises();
             for (int j=0; j<exeList.size(); j++) {
                 dbhandler.addExerciseInWorkout(exeList.get(j),wodList.get(i));
+                ExerciseDetail exedet = new ExerciseDetail(exeList.get(j).getName(),1);
+                dbhandler.addOrUpdateExercise(exedet);
             }
         }
 
     }
-
 
 
     public ArrayList<Workout> addEntryLevel(ArrayList<Workout> wodList) {
@@ -479,7 +660,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     public ArrayList<Workout> addCardio(ArrayList<Workout> wodList) {
 
         Exercise e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12;
@@ -546,6 +726,7 @@ public class MainActivity extends AppCompatActivity {
         return wodList;
 
     }
+
 
     public ArrayList<Workout> addPushups(ArrayList<Workout> wodList) {
 
@@ -731,7 +912,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     public ArrayList<Workout> addPlankPushups(ArrayList<Workout> wodList) {
 
         Exercise e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,e16,e17,e18,e19,e20;
@@ -868,7 +1048,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     public ArrayList<Workout> addMorningPushups(ArrayList<Workout> wodList) {
 
         Exercise e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12;
@@ -964,6 +1143,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     public ArrayList<Workout> addFrasFridayClassic(ArrayList<Workout> wodList) {
 
         Exercise e1,e2,e3,e4,e5,e6,e7;
@@ -990,6 +1170,7 @@ public class MainActivity extends AppCompatActivity {
         return wodList;
 
     }
+
 
     public ArrayList<Workout> addFridayClassic(ArrayList<Workout> wodList) {
 
@@ -1064,6 +1245,7 @@ public class MainActivity extends AppCompatActivity {
         return wodList;
 
     }
+
 
     public ArrayList<Workout> addPureHIIT(ArrayList<Workout> wodList) {
 
